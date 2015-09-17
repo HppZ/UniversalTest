ad989ef62899a5af7e6909dfdf22101ed61d4fb2
Hp <haopeng@camera360.com>
2015-09-17 18:37:02 +0800

    6

--- UniversalTest/Pages/BlankPage1.xaml.cs
+++ UniversalTest/Pages/BlankPage1.xaml.cs
@@ -28,6 +28,9 @@ namespace UniversalTest
     {
         private ObservableCollection<ImageItem> _source;
         private MainController mainController;
+
+        private ScrollBar _scrollBar;
+        private ScrollViewer _scrollViewer;
         public BlankPage1()
         {
             this.InitializeComponent();
@@ -35,16 +38,48 @@ namespace UniversalTest
             mainController = new MainController();
             _source = mainController.Source;
             this.DataContext = _source;
+
+            this.SizeChanged += BlankPage1_SizeChanged;
             Loaded += BlankPage1_Loaded;
+        }
+
+        private void BlankPage1_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (_scrollBar != null && _scrollViewer != null)
+            {
+                double s = _scrollViewer.ViewportHeight;
+                s = 30;
+                double max = _scrollBar.Maximum;
+                double min = _scrollBar.Minimum;
+                double t = s*s - 4*(min - max);
+                double b24ac = Math.Sqrt(t);
+                var r = (b24ac + s) / 2;
+
+                _scrollBar.ViewportSize = r;
+                Debug.WriteLine("viewpostsize "+r);
+            }
         }
 
         private void BlankPage1_Loaded(object sender, RoutedEventArgs e)
         {
-            DebugHelper.PrintTimeNow();
             mainController.Init();
-            DebugHelper.PrintTimeNow();
         }
 
+        private void UIElement_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            //Debug.WriteLine(_scrollViewer.ScrollableHeight);
+            //Debug.WriteLine(_scrollBar.ViewportSize);
+        }
+
+        private void ScrollViewer_Loaded(object sender, RoutedEventArgs e)
+        {
+            _scrollViewer = sender as ScrollViewer;
+        }
+
+        private void VerticalScrollBar_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _scrollBar = sender as ScrollBar;
+        }
 
     }
 }
