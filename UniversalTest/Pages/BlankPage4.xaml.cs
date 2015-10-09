dfbafb67a1fec22aa0cd40a26c41e5c1a86e0d69
Hp <haopeng@camera360.com>
2015-10-09 16:53:39 +0800

    slider bug?

--- UniversalTest/Pages/BlankPage4.xaml.cs
+++ UniversalTest/Pages/BlankPage4.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -22,13 +23,28 @@ namespace UniversalTest.Pages
     /// </summary>
     public sealed partial class BlankPage4 : Page
     {
-        /// <summary>
-        /// slider 做 圆球滚动条
-        /// </summary>
-
         public BlankPage4()
         {
             this.InitializeComponent();
         }
+
+
+        //--------------------------------------------------------------------------
+        /// <summary>
+        /// 这里发现: 对slider快速上下滑动, 放开, 再迅速快速滑动,再放开,重复操作会发生在拖动滚动条时_thumb?.IsDragging为false
+        /// </summary>
+        private int i = 0;
+        private void RangeBase_OnValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            Debug.WriteLine("value " + i++ + " drag " + _thumb?.IsDragging);
+        }
+
+        private Thumb _thumb;
+        private void VerticalThumb_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _thumb = sender as Thumb;
+        }
+        //--------------------------------------------------------------------------
+
     }
 }
