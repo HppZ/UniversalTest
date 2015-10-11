480b59655c57de5172ea44b4ba0fef55126836c2
hp <haopeng@camera360.com>
2015-10-11 22:30:24 +0800

    添加region

--- UniversalTest/Control/ScrollViewer/ThreeImageViewer.xaml.cs
+++ UniversalTest/Control/ScrollViewer/ThreeImageViewer.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 using UniversalTest.Control.ScrollViewer.Children;
 using UniversalTest.Controller;
@@ -21,12 +22,15 @@ namespace UniversalTest.Control.ScrollViewer
 {
     public sealed partial class ThreeImageViewer : UserControl
     {
+        #region Fields
         private MainController _mainController;
         private List<ImageItem> _images;
         private const double IMAGE_WH_RATIO = 4.0 / 3; // 照片宽高比
         private const double SCALE_RATIO = 0.8; // 缩放比例
         private const double SIDE_DISTANCE_RATIO = 5.0 / 16; // 两边间距占Image的比例
+        #endregion
 
+        #region Ctor
         public ThreeImageViewer()
         {
             this.InitializeComponent();
@@ -34,30 +38,16 @@ namespace UniversalTest.Control.ScrollViewer
             this.SizeChanged += ThreeImageViewer_SizeChanged;
             Loaded += ThreeImageViewer_Loaded;
         }
+        #endregion
 
-        private void ThreeImageViewer_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            UpdateImagesSize(e.NewSize);
-        }
-
-        private void UpdateImagesSize(Size newSize)
-        {
-            foreach (var child in CanvasContainer.Children)
-            {
-                var ele = (child as FrameworkElement);
-                ele.Height = newSize.Height;
-                ele.Width = ele.Height * IMAGE_WH_RATIO;
-            }
-
-            var childWidth = (CanvasContainer.Children[0] as ViewerItem).Width;
-            CanvasContainer.Width = childWidth + SIDE_DISTANCE_RATIO * childWidth * 2;
-        }
-
+        #region Loaded
         private void ThreeImageViewer_Loaded(object sender, RoutedEventArgs e)
         {
             Init();
         }
+        #endregion
 
+        #region Init
         /// <summary>
         /// 初始化
         /// </summary>
@@ -73,27 +63,21 @@ namespace UniversalTest.Control.ScrollViewer
                 child.RenderTransform = new CompositeTransform();
                 child.RenderTransformOrigin = new Point(0.5, 0.5);
                 (child as ViewerItem).Source = _images[i].PreviewImage;
-                UpdatePosition(child);
+                InitPosition(child);
             }
         }
 
-        private void UpdateChildrenPosition()
-        {
-            foreach (var child in CanvasContainer.Children)
-            {
-                UpdatePosition(child);
-            }
-        }
-
-        private void UpdatePosition(UIElement element)
+        /// <summary>
+        /// 更新子项位置
+        /// </summary>
+        private void InitPosition(UIElement element)
         {
             var center = CanvasContainer.ActualWidth / 2;
-
             var item = element as FrameworkElement;
             var trans = item.RenderTransform as CompositeTransform;
             if (item.Tag.ToString() == "0")
             {
-                Canvas.SetLeft(element, center - item.ActualWidth/2 - item.ActualWidth*SIDE_DISTANCE_RATIO);
+                Canvas.SetLeft(element, center - item.ActualWidth / 2 - item.ActualWidth * SIDE_DISTANCE_RATIO);
                 trans.ScaleX = SCALE_RATIO;
                 trans.ScaleY = SCALE_RATIO;
             }
@@ -103,7 +87,7 @@ namespace UniversalTest.Control.ScrollViewer
             }
             else if (item.Tag.ToString() == "2")
             {
-                Canvas.SetLeft(element, center - item.ActualWidth/2 + item.ActualWidth * SIDE_DISTANCE_RATIO);
+                Canvas.SetLeft(element, center - item.ActualWidth / 2 + item.ActualWidth * SIDE_DISTANCE_RATIO);
                 trans.ScaleX = SCALE_RATIO;
                 trans.ScaleY = SCALE_RATIO;
                 Canvas.SetZIndex(item, -1);
@@ -113,7 +97,43 @@ namespace UniversalTest.Control.ScrollViewer
                 item.Opacity = 0;
             }
         }
+        #endregion
 
+        #region Auto Layout
+        private void ThreeImageViewer_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateImagesSize(e.NewSize);
+        }
+
+        /// <summary>
+        /// 更新子项大小
+        /// </summary>
+        private void UpdateImagesSize(Size newSize)
+        {
+            foreach (var child in CanvasContainer.Children)
+            {
+                var ele = (child as FrameworkElement);
+                ele.Height = newSize.Height;
+                ele.Width = ele.Height * IMAGE_WH_RATIO;
+            }
+
+            var childWidth = (CanvasContainer.Children[0] as ViewerItem).Width;
+            CanvasContainer.Width = childWidth + SIDE_DISTANCE_RATIO * childWidth * 2;
+        }
+
+        #endregion
+
+        #region Change
+        /// <summary>
+        /// 更新所有子项位置
+        /// </summary>
+        private void GotoPreOrNext()
+        {
+        }
+        #endregion
+
+        #region Interaction
+        //-------------------------------------------------------------------
         private void Left_Tapped(object sender, RoutedEventArgs e)
         {
 
@@ -123,5 +143,6 @@ namespace UniversalTest.Control.ScrollViewer
         {
 
         }
+        #endregion
     }
 }
