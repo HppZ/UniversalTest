6d4c4350bd3e60684cf886ca006a84e372f3f52f
Hp <haopeng__@outlook.com>
2015-12-09 16:04:06 +0800

    层级控件 ---- 添加必要控件文件

---
+++ UniversalTest/Control/Menu/Child/CascadeMenuItem.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UniversalTest.Control.Menu.Child
+{
+    public sealed partial class CascadeMenuItem : UserControl
+    {
+        public CascadeMenuItem()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
