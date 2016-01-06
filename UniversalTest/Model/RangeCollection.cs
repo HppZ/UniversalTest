c1d2eeaae6191a52c03dcdac9e17e5b2033b4dcf
Hp <haopeng__@outlook.com>
2016-01-06 16:20:15 +0800

    report range change

--- UniversalTest/Model/RangeCollection.cs
+++ UniversalTest/Model/RangeCollection.cs
@@ -1,23 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
 
 namespace UniversalTest.Model
 {
     /// <summary>
     /// impl IItemsRangeInfo
     /// </summary>
-    public class RangeCollection: IItemsRangeInfo
+    public class RangeCollection<T> : ObservableCollection<T>, IItemsRangeInfo
     {
+        
+        public RangeCollection(IEnumerable<T> collection):base(collection)
+        {
+        }
+
         public void Dispose()
         {
+
         }
 
         public void RangesChanged(ItemIndexRange visibleRange, IReadOnlyList<ItemIndexRange> trackedItems)
         {
+            Debug.WriteLine("first "+visibleRange.FirstIndex +"  last " + visibleRange.LastIndex);
         }
     }
 }
---
+++ UniversalTest/Pages/BlankPage8.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using UniversalTest.Controller;
+using UniversalTest.Model;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UniversalTest.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BlankPage8 : Page
+    {
+        private MainController _mainController;
+        private RangeCollection<ImageItem> _imageItems;
+        public BlankPage8()
+        {
+            this.InitializeComponent();
+            _mainController = new MainController();
+            Loaded += BlankPage8_Loaded;
+        }
+
+        private async void BlankPage8_Loaded(object sender, RoutedEventArgs e)
+        {
+            await _mainController.Init();
+            _imageItems = new RangeCollection<ImageItem>(_mainController.Source);
+            DataContext = _imageItems;
+        }
+    }
+}
