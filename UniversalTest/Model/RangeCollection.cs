a663b67689c0cc574820140b4071d25825c11f9f
Hp <haopeng__@outlook.com>
2016-01-06 16:01:01 +0800

    rangeCollection

---
+++ UniversalTest/Model/RangeCollection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace UniversalTest.Model
+{
+    /// <summary>
+    /// impl IItemsRangeInfo
+    /// </summary>
+    public class RangeCollection: IItemsRangeInfo
+    {
+        public void Dispose()
+        {
+        }
+
+        public void RangesChanged(ItemIndexRange visibleRange, IReadOnlyList<ItemIndexRange> trackedItems)
+        {
+        }
+    }
+}
